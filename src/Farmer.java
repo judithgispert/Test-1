c7cbfa8c43f461545e499249b1eb26724dccdaba
judithgispert <jugispert@gmail.com>
2023-12-04 00:16:37 +0100

    Corrections farmer class

--- src/Farmer.java
+++ src/Farmer.java
@@ -1,16 +1,32 @@
-
 public class Farmer extends Seller{
 
+	private static int itemWear;
+	private final static double tax = 0.02;
 	
 	public Farmer(String name, String city) {
 		super(name, city);
-		
+		maxItems = 5;
+		itemWear = 15;
+	}
+	
+	public int getMaxItems() {
+		return maxItems;
+	}
+	
+	public int getItemWear() {
+		return itemWear;
 	}
 
 	@Override
-	public double itemTaxes(double price) {
-		double totalPrice = (price * 0.02) + price;
+	public double totalPrice(double price) {
+		double totalPrice = (price * tax) + price;
 		return totalPrice;
+	}
+
+	@Override
+	public int wearItem(Item item) {
+		itemWear =  item.getConservation() - itemWear;
+		return itemWear;
 	}
 	
 }

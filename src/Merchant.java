49f27197213d82c23c61a94e2baac78896ab206a
judithgispert <jugispert@gmail.com>
2023-12-04 00:17:09 +0100

    Corrections merchant class

--- src/Merchant.java
+++ src/Merchant.java
@@ -1,16 +1,25 @@
-
 public class Merchant extends Seller{
-
+	
+	private final static double tax = 0.04;
 	
 	public Merchant(String name, String city) {
 		super(name, city);
-		maxItems = 4;
+		maxItems = 7;
+	}
+	
+	public int getMaxItems() {
+		return maxItems;
 	}
 
 	@Override
-	public double itemTaxes(double price) {
-		double totalPrice = (price * 0.04) + price;
+	public double totalPrice(double price) {
+		double totalPrice = (price * tax) + price;
 		return totalPrice;
 	}
 
+	@Override
+	public int wearItem(Item item) {
+		int wear =  item.getConservation();
+		return wear;
+	}
 }

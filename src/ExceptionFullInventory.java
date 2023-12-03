b43cfbbb71524380445084e2fab795a84487f505
judithgispert <jugispert@gmail.com>
2023-12-03 22:06:20 +0100

    Exception message

---
+++ src/ExceptionFullInventory.java
@@ -0,0 +1,9 @@
+
+public class ExceptionFullInventory extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+
+	public ExceptionFullInventory (String message) {
+		super(message);
+	}
+}

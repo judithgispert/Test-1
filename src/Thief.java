571b2ec85c3b3f497c6d4a0d385d712cdcf0c3a2
judithgispert <jugispert@gmail.com>
2023-12-04 00:17:37 +0100

    Corrections thief class

--- src/Thief.java
+++ src/Thief.java
@@ -1,10 +1,32 @@
 
+
 public class Thief extends Seller{
+	
+	public static int itemWear;
 
 	public Thief(String name, String city) {
 		super(name, city);
 		maxItems = 3;
+		itemWear = 25;
 	}
+	
+	public int getMaxItems() {
+		return maxItems;
+	}
+
+	@Override
+	public double totalPrice(double price) {
+		double totalPrice = price;
+		return totalPrice;
+	}
+
+	@Override
+	public int wearItem(Item item) {
+		itemWear =  item.getConservation() - itemWear;
+		return itemWear;
+	}
+
+	
 
 	
 	

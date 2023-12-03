102fa3edadad0464ebc35b2ab1f16ec181c74c96
judithgispert <jugispert@gmail.com>
2023-12-04 00:16:47 +0100

    Main app

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 
 public class Main {
 	
-	public static void main (String[]args) throws ExceptionItem {
+	public static void main (String[]args){
 		
 		Menu menuApp = new Menu();
 		menuApp.app();
